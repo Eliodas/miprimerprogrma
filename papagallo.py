7969d3e2add8355891bf2d09cd49aebac26cb3ac
Eliodas <ijperez32@gmail.com>
2018-07-11 21:02:53 +0200

    nuevas pruebas

---
+++ papagallo.py
@@ -0,0 +1,11 @@
+
+
+number_to_guess= 4
+chance=1
+user_number=int(input("Dime un numero: "))
+
+if number_to_guess==user_number:
+    print("Has ganado")
+else if chance < 5:
+    print("Vuelve a intentarlo")
+    user_number = int(input("Dime un numero: ")
