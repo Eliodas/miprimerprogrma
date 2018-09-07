d9f0dd583d29bd11c623b9e53bd199867f5df278
eliodas <ijperez32@gmail.com>
2018-09-07 15:40:32 +0200

    Mejora de la lista

--- lista_compra.py
+++ lista_compra.py
@@ -1,20 +1,19 @@
 from builtins import print
 
 mi_lista=[]
-input_usuario=""
 numero_lista=0
 
 input_usuario=input("Que quieres comprar?: (Escriba FIN para acabar)")
-mi_lista.append(input_usuario)
+
 
 while input_usuario.upper() != "FIN":
-
-    input_usuario=input("Que quieres comprar?: (Escriba FIN para acabar)")
     mi_lista.append(input_usuario)
+    input_usuario=input("Que quieres comprar?: (Escriba FIN para acabar)")
 
-if len(mi_lista)!=1:
+
+if len(mi_lista)!=0:
     print("Tengo que comprar:")
 
-while numero_lista < len(mi_lista)-1:
+while numero_lista < len(mi_lista):
     print(mi_lista[numero_lista])
     numero_lista+=1
